7588e094944c32b58faf4927bebe6352b1dadcca
georgeazr <george.azr@gmail.com>
2022-04-16 21:09:44 -0300

    outro teste

---
+++ new2.py
@@ -0,0 +1,13 @@
+x =input(str("Você é o que? "))
+while x != "feio":
+    print("Errou!")
+    x = input(str("tente denovo "))
+while x == "feio":
+        print("Acertou mizeravel!")
+        break
+    
+    
+
+
+
+
